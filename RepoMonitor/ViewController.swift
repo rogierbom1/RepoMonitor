be23e8ca58e19020566a2643d6078ce3e607d11d
Rogier Bom <rogier@rogierbom.nl>
2015-10-21 19:47:41 +0200

    Initial Commit

---
+++ RepoMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RepoMonitor
+//
+//  Created by Rogier Bom on 21-10-15.
+//  Copyright © 2015 Rogier Bom. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
